cec1e1c160ca416a2cb873f7fc78b9e7013502d3
Yakha12 <89409148+Yakha12@users.noreply.github.com>
2023-05-15 08:56:44 +0200

    Add GamePanel methods skeletons

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
 
+//this basically the game window
 public class GameFrame extends JFrame
 {
-    GameFrame(){}
+    GameFrame()
+    {
+        //Create an instance of Game panel to add to game frame
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();// will fit the Frame around all added components
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
