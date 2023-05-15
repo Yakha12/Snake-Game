c952f872ef9f9cfcd4dfa86a722d90a92bafac01
Yakha12 <89409148+Yakha12@users.noreply.github.com>
2023-05-15 11:25:02 +0200

    Define Draw and Move Functions

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -45,13 +45,61 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void paintComponent(Graphics g){
-
+        super.paintComponent(g);
+        draw(g);
     }
     public void draw(Graphics g){
 
+        //visualise the game grid FOR DEV
+        for (int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
+            g.drawLine(0,i * UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
+        }
+        //DRAW APPLE
+        g.setColor(Color.GREEN);
+        //draw circle
+        g.fillOval(appleX,appleY,UNIT_SIZE,UNIT_SIZE);
+
+        //DRAW SNAKE
+        for (int i = 0; i<bodyParts;i++){
+            //i = 0 is where the head is located
+            if(i==0){
+                g.setColor(Color.MAGENTA);
+                g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+            }
+            else {
+                g.setColor(Color.magenta);
+                g.fillRect(x[i],y[i],UNIT_SIZE,UNIT_SIZE);
+            }
+        }
     }
-    public void newApple(){}
+    public void newApple(){
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int) (SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+
+    }
     public  void move() {
+        //For loop to iterate through the Snake's bodyparts
+        for (int i = bodyParts;i>0;i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        //move Snake
+        switch (direction){
+            case 'U':
+                y[0]= y[0]-UNIT_SIZE;
+                break;
+
+            case 'D':
+                y[0] = y[0]+UNIT_SIZE;
+                break;
+            case 'L':
+                x[0]= x[0]-UNIT_SIZE;
+                break;
+
+            case 'R':
+                x[0] = x[0]+UNIT_SIZE;
+        }
 
     }
     public void  checkApple(){
