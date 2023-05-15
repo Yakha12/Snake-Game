b3dde22d59f4f157bf9d814137251611f249db7d
Yakha12 <89409148+Yakha12@users.noreply.github.com>
2023-05-15 09:35:27 +0200

    Add GamePanel fields

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -6,6 +6,28 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+
+    //How big should the object in game be
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNIT = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+   //The Higher the number for delay the slower the game
+    static final int DELAY = 75;
+
+    //Fields for Game Objects manipulation
+    //Array to Hold body co-ordinates for the Snake We use GAME UNIT because the snake will never be larger than the game
+
+    final int  x[] = new int[GAME_UNIT];
+    final int  y[] = new int[GAME_UNIT];
+    int bodyParts = 6;
+    int appleEaten;
+    int appleX,appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
     GamePanel(){
 
     }
