6b50ac0b1bf888fdf3f0cbf2c2c9432bfa83cbaf
Yakha12 <89409148+Yakha12@users.noreply.github.com>
2023-05-15 08:28:07 +0200

    Create instance of GameFrame class

---
+++ src/GamePanel.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GamePanel extends JPanel implements ActionListener {
+    GamePanel(){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
---
+++ src/SnakeGame.java
@@ -0,0 +1,10 @@
+public class SnakeGame {
+
+    public static void main(String[] arg)
+    {
+        //Creates an instance of Gameframe
+        new GameFrame();
+
+
+    }
+}
